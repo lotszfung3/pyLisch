2355c94ec632fc98711636fd017896b59e6b6eeb
manyu1234 <17606618+manyu1234@users.noreply.github.com>
2018-02-15 21:33:56 +0800

    Add files via upload

--- pyLisch/program.py
+++ pyLisch/program.py
@@ -1,45 +1,59 @@
 from pyLisch.node import Node,PrimNode,OperNode
 from pyLisch.utils import split_str_to_list
 class Program:
-    def __init__(self,string):
-        self.eval_list=[]
-        self.def_list=[]
-        for substr in split_str_to_list(string):
-            node=Node.buildTree(substr)
-            if(node.value=="define"):
-                self.def_list.append(node)
-            else:
-                self.eval_list.append(node)
-    def replace_node(self,node):
-        '''
-        replace every single variable node in eval_list
-        '''
-        for i in self.eval_list:
-            
-    def get_func_node(self,name,arg_list):
-        '''
-        get new node
-        input: name=sq, arg_list=[PrimNode:3]
-        output: [OperNode:*] with two PrimNode children of value 3 
-        '''
-        for defNode in self.def_list:
-            if str(defNode.child_list[0])==name:
-                assert len(arg_list)==len(defNode.child_list[1])
-                new_node=defNode.child_list[1].copyTree()# OperNode:*
-        return new_node
-    def replace_node(self,node,ctx):
-        '''
-        node [OperNode:*] with two child Node of value x
-        ctx: ["x":3]
-        output: [OperNode:*] with two child Node of value 3
-        '''
-        for i,n in enumerate(node.child_list):
-            if()
-        
-        
-        
-        
-    def run(self):
-        for i in self.eval_list:
-            yield round(i.eval_node(),8)
-    
+	def __init__(self,string):
+		self.eval_list=[]
+		self.def_list=[]
+		for substr in split_str_to_list(string):
+			node=Node.buildTree(substr)
+			if(node.value=="define"):
+				self.def_list.append(node)
+			else:
+				self.eval_list.append(node)
+				
+	def replace_node(self,node):
+		'''
+		replace every single variable node in eval_list
+		'''
+		for i in self.eval_list:
+			pass
+			
+	def get_func_node(self,name,arg_list):
+		'''
+		get new node
+		input: name=sq, arg_list=[PrimNode:3]
+		output: [OperNode:*] with two PrimNode children of value 3 
+		'''
+		for defNode in self.def_list:
+			if str(defNode.child_list[0])==name:
+				assert len(arg_list)==len(defNode.child_list[1])
+				new_node=defNode.child_list[1].copyTree()# OperNode:*
+		return new_node
+	def replace_node(self,node,ctx):
+		'''
+		node [OperNode:*] with two child Node of value x
+		ctx: ["x":3]
+		output: [OperNode:*] with two child Node of value 3
+		'''
+		for i,n in enumerate(node.child_list):
+			pass
+		
+	def eval_node(self,node):
+		'''
+		input:
+			node: node to be replaced
+		output:
+			node created by node.value
+		'''
+
+		for defNode in self.def_list:
+			if (defNode.child_list[0].value==node.value):
+				assert(len(defNode.child_list[0])==len(node.child_list))
+				return defNode.child_list[1].eval_node()
+		assert(False)   
+		
+		
+	def run(self):
+		for i in self.eval_list:
+			yield round(i.eval_node(),8)
+	
--- pyLisch/utils.py
+++ pyLisch/utils.py
@@ -1,29 +1,29 @@
 import re
 def split_str_to_list(string):
-    string=strip_str(string)
-    if("(" not in string):
-        return string.split()
-    res_list=[]
-    prev_idx=-1
-    bracket_counter=0
-    for i in range(len(string)):
-        if(bracket_counter==0 and string[i]==" " and prev_idx<i):
-            res_list.append(string[prev_idx+1:i])
-            prev_idx=i
-        elif string[i]=="(":
-            bracket_counter+=1
-            if(bracket_counter==1):#start of next procedure
-                prev_idx=i
-        elif string[i]==")":
-            bracket_counter-=1
-            if(bracket_counter==0):#end of this procedure
-                res_list.append(string[prev_idx+1:i])
-                prev_idx=i+1
-        elif (i==len(string)-1 and prev_idx<i):
-            res_list.append(string[prev_idx+1:])
-        assert(bracket_counter>=0)
-    assert(bracket_counter==0)
-    return res_list
+	string=strip_str(string)
+	if("(" not in string):
+		return string.split()
+	res_list=[]
+	prev_idx=-1
+	bracket_counter=0
+	for i in range(len(string)):
+		if(bracket_counter==0 and string[i]==" " and prev_idx<i):
+			res_list.append(string[prev_idx+1:i])
+			prev_idx=i
+		elif string[i]=="(":
+			bracket_counter+=1
+			if(bracket_counter==1):#start of next procedure
+				prev_idx=i
+		elif string[i]==")":
+			bracket_counter-=1
+			if(bracket_counter==0):#end of this procedure
+				res_list.append(string[prev_idx+1:i])
+				prev_idx=i+1
+		elif (i==len(string)-1 and prev_idx<i):
+			res_list.append(string[prev_idx+1:])
+		assert(bracket_counter>=0)
+	assert(bracket_counter==0)
+	return res_list
 def strip_str(string):
-    return re.sub(" +"," ",string.strip(" \n\t").replace("\n"," "))
-    
+	return re.sub(" +"," ",string.strip(" \n\t").replace("\n"," "))
+	
